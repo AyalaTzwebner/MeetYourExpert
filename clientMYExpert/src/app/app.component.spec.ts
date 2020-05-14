0e9ef7e92c6e2b62d2854d9af99199f8e895c398
MeetYourExpert <ayalash1135@gmail.com>
2020-05-14 17:10:48 +0300

    project init

--- clientMYExpert/src/app/app.component.spec.ts
+++ clientMYExpert/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clientMeetYE'`, () => {
+  it(`should have as title 'clientMYExpert'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clientMeetYE');
+    expect(app.title).toEqual('clientMYExpert');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('clientMeetYE app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('clientMYExpert app is running!');
   });
 });
